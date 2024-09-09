1d547849b2ec59993c29f5e35ea74a4243432bdf
ishank <ishankdev@gmail.com>
2024-09-08 23:43:52 -0700

    updated comments

--- CECS_524_APL/Unit_1_Assignment_1.py
+++ CECS_524_APL/Unit_1_Assignment_1.py
@@ -4,7 +4,8 @@ def interpret(code):
     ptr_tape = 0  # pointer on the tape
     i = 0  # another pointer to read the string
     bracket_mapper = {}  # Bracket map for jumps
-    # Bracket map to handle matching '[' and ']', using map for O(1) fast retreival instead of nested loop
+    input_buffer = []  # Buffer to handle multi-character input
+    # Bracket map to handle matching '[' and ']', using map for O(1) fast retrieval instead of nested loop
     stack = []
     for idx, char in enumerate(code):
         if char == '[':
@@ -13,33 +14,27 @@ def interpret(code):
             start = stack.pop()
             bracket_mapper[start] = idx
             bracket_mapper[idx] = start
-#    print(bracket_mapper)
+    
     while i < len(code):
-        
         if code[i] == '>':
-            ptr_tape = (ptr_tape + 1) 
-
+            ptr_tape = (ptr_tape + 1) % 1000  # Prevents going out of bounds
         elif code[i] == '<':
-            ptr_tape = (ptr_tape - 1) 
-
+            ptr_tape = (ptr_tape - 1) % 1000
         elif code[i] == '+':
-            memory_tape[ptr_tape] = (memory_tape[ptr_tape] + 1)
-
+            memory_tape[ptr_tape] = (memory_tape[ptr_tape] + 1) % 256  # Ensures memory value is between 0-255 for wrap arounds
         elif code[i] == '-':
-            memory_tape[ptr_tape] = (memory_tape[ptr_tape] - 1)
-
+            memory_tape[ptr_tape] = (memory_tape[ptr_tape] - 1) % 256 
         elif code[i] == '.':
             print(chr(memory_tape[ptr_tape]), end='')
-
         elif code[i] == ',':
-            memory_tape[ptr_tape] = ord(input()[0])
-            
-
+            if not input_buffer:  # If the buffer is empty, request input
+                input_buffer = list(input("Input: "))  # Take input as a string
+            if input_buffer:
+                memory_tape[ptr_tape] = ord(input_buffer.pop(0))  # Take one character at a time from the buffer
         elif code[i] == '[':
             # move to end of matching closed bracket if pointer is pointing to zero bytes
             if memory_tape[ptr_tape] == 0:
                 i = bracket_mapper[i]
-
         elif code[i] == ']':
             # move to starting of matching open bracket if pointer is pointing to non zero bytes
             if memory_tape[ptr_tape] != 0:
@@ -48,6 +43,7 @@ def interpret(code):
 
 if __name__ == "__main__":
     code = input("Welcome to the BrainFu*k Interpreter\nPlease Enter the Brainfu*k code: ")
+    # Sample Test
     #++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.
     print("Interpreter Output:")
     interpret(code)
